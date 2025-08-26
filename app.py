57d28bb527562b65cc4b87d60bfe7ae491569432
SJOEc <115746349+SJOEC@users.noreply.github.com>
2025-08-26 16:20:16 +0000

    resolve games names and genres

--- app.py
+++ app.py
@@ -10,11 +10,25 @@ API_KEY = data['API_KEY']
 print(API_KEY)
 
 if __name__ == '__main__':
+    user_games_data = []
+    porn = []
+
     steam_url = 'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/'
     vanity_url = 'Bl4ky113'
 
     user_steam_id = utils.get_steam_id(vanity_url, API_KEY)
     user_games = utils.get_games_stats(user_steam_id, API_KEY)
+
+    print(user_steam_id)
+    
+    for game in user_games:
+        user_games_data.append(utils.get_game_data(game['appid']))
+
+    for game in user_games_data:
+        if utils.is_porn(game):
+            porn.append(game['name'])
+
+    print(porn)
 
     print(user_games)
 
--- utils.py
+++ utils.py
@@ -11,3 +11,36 @@ def get_games_stats(steam_id, API_KEY, steam_url = 'http://api.steampowered.com/
     response = requests.get(steam_url, params=params)
     return response.json()['response']['games']
 
+def get_game_data(game_id, steam_url='https://store.steampowered.com/api/appdetails'):
+    
+    relevant_app_details = {}
+
+    params = {'appids': game_id}
+    response = requests.get(steam_url, params=params)
+    data = response.json()
+    
+    if data[str(game_id)]['success']:
+
+        try:
+            relevant_app_details = {
+                "name": data[str(game_id)]['data']['name'],
+                "genres": data[str(game_id)]['data']['genres'],
+                #"ratings": data[str(game_id)]['data']['ratings'],
+                "categories": data[str(game_id)]['data']['categories'],
+                "required_age": data[str(game_id)]['data']['required_age']
+            }
+        except KeyError as e:
+            print(f"El juego {game_id} no tiene la clave: {e}")
+            relevant_app_details = {
+                "name": data[str(game_id)]['data']['name'],
+                "genres": None,
+                "categories": data[str(game_id)]['data'].get('categories', None),
+                #"ratings": data[str(game_id)]['data'].get('ratings', None),
+                "required_age": data[str(game_id)]['data'].get('required_age', None)
+            }
+
+    return relevant_app_details
+
+def is_porn(game_data):
+    if int(game_data.get('required_age', 0)) >= 18:
+        return True
